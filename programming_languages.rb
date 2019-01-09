00c702cf96a7961d94a0d37ad0d1780c2ef556d3
Dylan Clements <dclements@protonmail.com>
2019-01-09 18:25:37 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,13 +1,13 @@
 def reformat_languages(languages)
-    new_hash = {}
-    languages.each do |styles, data|
-      data.each do |language, type|
-        if new_hash.has_key?(language)
-          new_hash[language][:style] << styles
-        else
-          new_hash[language] = {
-            style: [styles],
-            type: type}
+ new_hash = {}
+ languages.each do |styles, data|
+   data.each do |language, type|
+     if new_hash.has_key?(language)
+      new_hash[language][:style] << styles
+     else
+      new_hash[language] = {
+       style: [styles],
+       type: type}
    end
   end
  end
